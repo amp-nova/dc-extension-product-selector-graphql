9cb9d54db4300d6331e4c625a5e80fa3b27a5cec
dave lilly <dave.lilly@gmail.com>
2021-03-30 15:37:44 -0500

    first commit

---
+++ src/backends/BigCommerce.js
@@ -0,0 +1,108 @@
+import { ProductSelectorError } from '../ProductSelectorError';
+import { setContext } from '@apollo/client/link/context';
+import { createHttpLink, ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import _ from 'lodash';
+
+let mapProduct = prod => ({
+  id: prod.id,
+  name: prod.name,
+  image: _.get(_.first(prod.variants), 'defaultImage.url')
+})
+
+export class BigCommerce {
+  client;
+  constructor({ backendId, graphqlUrl }) {
+    console.log(`BigCommerce init ${backendId}`)
+    const httpLink = createHttpLink({
+      uri: graphqlUrl
+    })
+
+    const authLink = setContext((_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          'X-Commerce-Backend-Key': backendId,
+        }
+      }
+    });
+
+    this.client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+    });
+  }
+
+  async getItemById(id) {
+    if (this.client) {
+      let results = await this.client.query({
+        query: gql`
+          query productById {
+            product(id:"${id}") {
+              id
+              name
+              variants {
+              defaultImage {
+                url
+              }
+            }
+          }
+        }`
+      })
+      return mapProduct(results.data.product)
+    }
+  }
+
+  async getItems(state, filterIds = []) {
+    try {
+      if (!filterIds.length) {
+        return [];
+      }
+
+      return await Promise.all(filterIds.map(await _.bind(this.getItemById, this)))
+    } catch (e) {
+      console.error(e);
+      throw new ProductSelectorError('Could not get items', ProductSelectorError.codes.GET_SELECTED_ITEMS);
+    }
+  }
+
+  async search(state) {
+    const {
+      searchText,
+      page,
+      PAGE_SIZE
+    } = state;
+
+    let results = await this.client.query({
+      query: gql`
+        query products {
+          products(keyword:"${searchText}",limit:${PAGE_SIZE},offset:${page.curPage * PAGE_SIZE}) {
+            total
+            results {
+              id
+              name
+              variants {
+                defaultImage {
+                  url
+                }
+              }
+            }
+          }
+        }
+      `
+    })
+
+    let items = _.map(results.data.products.results, mapProduct)
+    return {
+      items,
+      page: {
+        numPages: Math.ceil(results.data.products.total / PAGE_SIZE),
+        curPage: page.curPage,
+        total: results.data.products.total
+      }
+    };
+  }
+
+  exportItem(item) {
+    return item.id;
+  }
+}
